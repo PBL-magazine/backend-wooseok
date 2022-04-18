29a7cc2f26d57e859806fc5ba4ffc94882b627c0
moto4321 <moto432195@gmail.com>
2022-04-18 16:10:18 +0900

    signup validation test code

--- test/auth/auth.spec.js
+++ test/auth/auth.spec.js
@@ -1,4 +1,4 @@
-const { signInValidator } = require('../../src/auth/dto/auth.validation')
+const { signInValidator, signUpValidator } = require('../../src/auth/dto/auth.validation')
 
 describe('로그인 테스트', () => {
   test('로그인 형식 테스트 true', async () => {
@@ -30,6 +30,45 @@ describe('로그인 테스트', () => {
     
 
     await signInValidator(req, res, next);
+    expect(next).toBeCalledTimes(0);
+  
+  })
+
+});
+
+
+describe('회원가입 테스트', () => {
+  test('회원가입 형식 테스트 true', async () => {
+    const req = {
+      body: {
+        email: 'moto4321@naver.com',
+        nickname: 'wooseok',
+        password: 'aaaa1111'
+      }    
+    };
+    const res = {};
+    const next = jest.fn();
+
+    await signUpValidator(req, res, next);
+    expect(next).toBeCalledTimes(1);
+  })
+
+  test('회원가입 형식 테스트 false', async () => {
+    const req = {
+      body: {
+        email: 'moto4321@naver.com',
+        nickname: 'wd',
+        password: 'aaaa1111'
+      }    
+    };
+    const res = { //? 테스트할 함수안에 들어있는 실행 로직들은 꼼꼼하게 하나하나 선언해주는게 좋다. (에러방지) 
+      status: jest.fn(() => res), // 체이닝 하니까 자기자신을 리턴 
+      json: jest.fn(), 
+    };
+    const next = jest.fn();
+    
+
+    await signUpValidator(req, res, next);
     expect(next).toBeCalledTimes(0);
   
   })
